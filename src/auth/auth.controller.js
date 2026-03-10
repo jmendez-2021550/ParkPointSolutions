4bd4eb250cd566c950bf9267906de011ed0f0910
jaquino-2021159 <jaquino-2021159@kinal.edu.gt>
2026-03-10 15:41:31 -0600

    Se integró helper de registro de usuario

--- src/auth/auth.controller.js
+++ src/auth/auth.controller.js
@@ -1,18 +1,21 @@
 import {
-  registerUserHelper,
-  loginUserHelper,
-  verifyEmailHelper,
-  resendVerificationEmailHelper,
-  forgotPasswordHelper,
-  resetPasswordHelper,
+    registerUserHelper,
+    loginUserHelper,
+    verifyEmailHelper,
+    resendVerificationEmailHelper,
+    forgotPasswordHelper,
+    resetPasswordHelper,
 } from '../../helpers/auth-operations.js';
 
 import { getUserProfileHelper } from '../../helpers/profile-operations.js';
 import { asyncHandler } from '../../middlewares/server-genericError-handler.js';
 
 export const register = asyncHandler(async (req, res) => {
-      try {
-    const userData = {
-      ...req.body,
-      profilePicture: req.file ? req.file.path : null,
-    };
+    try {
+        const userData = {
+            ...req.body,
+            profilePicture: req.file ? req.file.path : null,
+        };
+
+        const result = await registerUserHelper(userData);
+        res.status(201).json(result);
