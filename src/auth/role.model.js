c4fb87c661ce0e981d1b1d78ea51c12338211355
jaquino-2021159 <jaquino-2021159@kinal.edu.gt>
2026-03-10 16:07:13 -0600

    Se agregaron importaciones de utilidades y constantes de roles

--- src/auth/role.model.js
+++ src/auth/role.model.js
@@ -1,2 +1,6 @@
 import { DataTypes } from 'sequelize';
 import { sequelize } from '../../configs/db.js';
+
+import { generateUserId } from '../../helpers/uuid-generator.js';
+import { User } from '../users/user.model.js';
+import { ALLOWED_ROLES } from '../../helpers/role-constants.js';
