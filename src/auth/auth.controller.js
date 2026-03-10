75f86b1a8764003d55aed08e75ef6764c336b8a1
jaquino-2021159 <jaquino-2021159@kinal.edu.gt>
2026-03-10 15:39:44 -0600

    Se inició implementación del controlador de registro

--- src/auth/auth.controller.js
+++ src/auth/auth.controller.js
@@ -9,3 +9,5 @@ import {
 
 import { getUserProfileHelper } from '../../helpers/profile-operations.js';
 import { asyncHandler } from '../../middlewares/server-genericError-handler.js';
+
+export const register = asyncHandler(async (req, res) => {
