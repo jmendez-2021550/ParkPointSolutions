fd09fb576856b1cf26248cd56d194f8fb8c9007e
jaquino-2021159 <jaquino-2021159@kinal.edu.gt>
2026-03-10 15:39:10 -0600

    Se agregó middleware asyncHandler para manejo de errores

--- src/auth/auth.controller.js
+++ src/auth/auth.controller.js
@@ -8,3 +8,4 @@ import {
 } from '../../helpers/auth-operations.js';
 
 import { getUserProfileHelper } from '../../helpers/profile-operations.js';
+import { asyncHandler } from '../../middlewares/server-genericError-handler.js';
