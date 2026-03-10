61511b0cff2dfde8d7cf3ae1fee55165c81223ce
jaquino-2021159 <jaquino-2021159@kinal.edu.gt>
2026-03-10 16:09:41 -0600

    Se inició definición del modelo Role

--- src/auth/role.model.js
+++ src/auth/role.model.js
@@ -4,3 +4,7 @@ import { sequelize } from '../../configs/db.js';
 import { generateUserId } from '../../helpers/uuid-generator.js';
 import { User } from '../users/user.model.js';
 import { ALLOWED_ROLES } from '../../helpers/role-constants.js';
+
+export const Role = sequelize.define(
+    'Role',
+    {
