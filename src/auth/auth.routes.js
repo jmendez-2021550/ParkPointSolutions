1df5fc99f0e4c6988c0fd56d0f5e857b21267ec9
jaquino-2021159 <jaquino-2021159@kinal.edu.gt>
2026-03-10 15:55:25 -0600

    Se agregaron middlewares de limitación de solicitudes

--- src/auth/auth.routes.js
+++ src/auth/auth.routes.js
@@ -1,3 +1,8 @@
 import { Router } from 'express';
 import * as authController from './auth.controller.js';
 import { validateJWT } from '../../middlewares/validate-JWT.js';
+
+import {
+    authRateLimit,
+    requestLimit,
+} from '../../middlewares/request-limit.js';
