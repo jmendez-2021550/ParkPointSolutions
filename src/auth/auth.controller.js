069156712ed713aa74c62d60ac7d240e5065102a
jaquino-2021159 <jaquino-2021159@kinal.edu.gt>
2026-03-10 15:40:32 -0600

    Se agregó preparación de datos del usuario en registro

--- src/auth/auth.controller.js
+++ src/auth/auth.controller.js
@@ -11,3 +11,8 @@ import { getUserProfileHelper } from '../../helpers/profile-operations.js';
 import { asyncHandler } from '../../middlewares/server-genericError-handler.js';
 
 export const register = asyncHandler(async (req, res) => {
+      try {
+    const userData = {
+      ...req.body,
+      profilePicture: req.file ? req.file.path : null,
+    };
