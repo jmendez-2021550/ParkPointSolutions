b9630895ba113ef456067cdd8e3b4e80ab6a405d
jaquino-2021159 <jaquino-2021159@kinal.edu.gt>
2026-03-10 15:37:40 -0600

    Se agregaron helpers de operaciones de autenticación

---
+++ src/auth/auth.controller.js
@@ -0,0 +1,8 @@
+import {
+  registerUserHelper,
+  loginUserHelper,
+  verifyEmailHelper,
+  resendVerificationEmailHelper,
+  forgotPasswordHelper,
+  resetPasswordHelper,
+} from '../../helpers/auth-operations.js';
