074bc74bd7e43ed29eb28ef60485804947f51a24
jaquino-2021159 <jaquino-2021159@kinal.edu.gt>
2026-03-10 16:11:55 -0600

    Se agregó modelo intermedio UserRole para relación entre usuarios y roles

--- src/auth/role.model.js
+++ src/auth/role.model.js
@@ -47,3 +47,50 @@ export const Role = sequelize.define(
         updatedAt: 'updated_at',
     }
 );
+export const UserRole = sequelize.define(
+    'UserRole',
+    {
+        Id: {
+            type: DataTypes.STRING(16),
+            primaryKey: true,
+            field: 'id',
+            defaultValue: () => generateUserId(),
+        },
+        UserId: {
+            type: DataTypes.STRING(16),
+            allowNull: false,
+            field: 'user_id',
+            references: {
+                model: User,
+                key: 'id',
+            },
+        },
+        RoleId: {
+            type: DataTypes.STRING(16),
+            allowNull: false,
+            field: 'role_id',
+            references: {
+                model: Role,
+                key: 'id',
+            },
+        },
+        CreatedAt: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+            field: 'created_at',
+        },
+        UpdatedAt: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+            field: 'updated_at',
+        },
+    },
+    {
+        tableName: 'user_roles',
+        timestamps: true,
+        createdAt: 'created_at',
+        updatedAt: 'updated_at',
+    }
+);
